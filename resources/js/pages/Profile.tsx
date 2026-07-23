de0a412a60507c4d8e16475b263b00dd155ad5a4
unknown <pogchampik132@interia.pl>
2026-07-23 10:53:20 +0200

    Added Charts Added Custom Weather Themes

--- resources/js/pages/Profile.tsx
+++ resources/js/pages/Profile.tsx
@@ -1,9 +1,10 @@
+import { CenterRow } from "@/components/utils/Center";
 import { GlassSelect } from "@/components/utils/GlassSelect";
 import { Glass1, GlassDark } from "@/components/utils/Morphisim";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { Value } from "@radix-ui/react-select";
 import { AnimatePresence,motion } from "framer-motion";
-import { ArrowLeft, ChevronDown, Clock4, Settings, Sidebar } from "lucide-react";
+import { ArrowLeft, ChevronDown, Clock4, RefreshCcwDot, Settings, Sidebar } from "lucide-react";
 import { useState } from "react";
 
 export default function Profile(){
@@ -11,8 +12,17 @@ export default function Profile(){
     const [temperatureUnit, setTemperatureUnit] =useLocalStorage("temperature","celsius");
     const [windUnit, setWindUnit] = useLocalStorage("wind","kmh");
     const [timeFormat, setTimeFormat] = useLocalStorage("time", "24h");
+    const [customTheme,setCustomTheme] =useLocalStorage('theme','default');
+    const [customBackground,setCustomBackground] =useLocalStorage('background',null);
 
-    
+    const handleImageUpload = (e) => {
+        const file = e.target.files?.[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+        reader.onloadend = () => setCustomBackground(reader.result);
+        reader.readAsDataURL(file);
+    };
     // console.log(temperatureUnit)
     return (
         <main className="relative h-dvh   overflow-hidden bg-[url('/weather/background/cloudsun.jpg')] bg-center bg-cover">
@@ -120,6 +130,43 @@ export default function Profile(){
                         ]}
                     />
                 </div>
+                <div>
+                    <p className="text-gray-300 mb-2">
+                        Tło pogody
+                    </p>
+
+                    <GlassSelect
+                        value={customTheme}
+                        onChange={setCustomTheme}
+                        options={[
+                            {
+                            label:"Wbudowane",
+                            value:"default"                       
+                            },
+                            {
+                            label:"Własne",
+                            value:"custom"
+                            },
+                        ]}
+                    />
+                   {customTheme === "custom" ? (
+                    <div>
+                        <p className="text-gray-300 mb-2">
+                            Tło pogody
+                            <Glass1>
+                               <input type="file"  accept="image/*" onChange={handleImageUpload}className="text-white text-sm" />
+                            </Glass1>
+                        </p>
+                    </div>
+                    ) : null}
+
+                </div>
+                
+                    <Glass1 className="p-2 w-[35%] h-12 rounded-4xl border-2 border-white/10  bg-brown-900/10 backdrop-blur-[3px] font-semibold">
+                        <CenterRow><button onClick={() => window.location.reload()} className="flex flex-row items-center justify-center gap-2  text-center mx-auto"><span>Odśwież</span><RefreshCcwDot className="inline-block"/></button></CenterRow>                    
+                    </Glass1>
+
+                
             </div>
 
           
--- resources/js/pages/Weather.tsx
+++ resources/js/pages/Weather.tsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from "react";
 import axios from "axios";
 // import {Header} from '@/components/Header'
-import { Wind,Droplet, Sun,Clock4,Calendar, ArrowLeft, Sidebar, ArrowDown, ChevronDown, TrashIcon, PlusIcon, LoaderCircle, Trash2Icon, X, Plus, Calendar1Icon, Calendar1 } from "lucide-react";
+import { Wind,Droplet, Sun,Clock4,Calendar, ArrowLeft, Sidebar, ArrowDown, ChevronDown, TrashIcon, PlusIcon, LoaderCircle, Trash2Icon, X, Plus, Calendar1Icon, Calendar1, Thermometer, Gauge, Navigation, CloudRainWind, CloudRain } from "lucide-react";
 import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { CenterAll, CenterRow, CenterX, CenterY } from "@/components/utils/Center";
 import { Glass1 } from "@/components/utils/Morphisim";
@@ -12,6 +12,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Header } from "@/components/header/Header";
 import { Navbar } from "@/components/Navbar/Navbar";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
+import {ResponsiveContainer,AreaChart,Area,XAxis,YAxis,Tooltip,CartesianGrid,} from "recharts";
 const defaultCity = {
     id:756135,
     name:"Warszawa",
@@ -32,6 +33,8 @@ export default function Weather(){
     const [windUnit, setWindUnit] = useLocalStorage("wind","kmh");
     const [timeFormat, setTimeFormat] = useLocalStorage("time", "24h");
     const [savedWeather, setSavedWeather]= useLocalStorage('savedWeather',null)
+    const [customTheme,setCustomTheme] =useLocalStorage('theme','default');
+    const [customBackground,setCustomBackground] =useLocalStorage('background',null);
 
 
     function normalize(a,b){
@@ -56,7 +59,7 @@ export default function Weather(){
                 data: response.data
                 })
         }
-            if(savedWeather){
+            if(savedWeather.data>0){
                 if(savedWeather.cityId === selectCity.id){
                     const now = new Date();
                     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate(),now.getHours(),now.getMinutes(),now.getSeconds());
@@ -126,7 +129,7 @@ export default function Weather(){
         return(
 
         
-        <div className="w-26 h-34 shrink-0 border-1 rounded-2xl flex flex-col justify-center items-center text-white" >
+        <div className="w-26 h-34 shrink-0 border-1 border-gray-600 rounded-2xl flex flex-col justify-center items-center text-white" >
             <img src={getWeatherConditionIcon(weather?.data?.hourly[index]?.weather_code)} alt="" className="w-16 object-contai"/>
            <span className="font-semibold text-xl"> {weather?.data?.hourly[index]?.temperature || <LoaderCircle className="animate-spin"size={12}/>}{weather?.data?.current.temperature_unit}</span>
             <span>{day}, {time}</span>
@@ -137,6 +140,7 @@ export default function Weather(){
         </div>
         )
     }
+    
     function TemperatureBar({temperature}){
         const min = -10;
         const max = 45;
@@ -209,16 +213,39 @@ export default function Weather(){
 
        
     }
+    const chartData = weather?.data?.hourly?.slice(0,24).map((item) => {
+    const timeLabel = new Date(item.time).toLocaleTimeString("pl-PL", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    return {
+      time: timeLabel,
+      temp: item.temperature,
+      rain: item.precipation,
+    };
+  }) || []
+  function getWindDirection(degrees){
+    if (degrees >= 337.5 || degrees < 22.5) return "N";
+    if (degrees >= 22.5 && degrees < 67.5) return "NE";
+    if (degrees >= 67.5 && degrees < 112.5) return "E";
+    if (degrees >= 112.5 && degrees < 157.5) return "SE";
+    if (degrees >= 157.5 && degrees < 202.5) return "S";
+    if (degrees >= 202.5 && degrees < 247.5) return "SW";
+    if (degrees >= 247.5 && degrees < 292.5) return "W";
+    if (degrees >= 292.5 && degrees < 337.5) return "NW";
+  }
+  const bgImage = customTheme === "custom" && customBackground
+  ? customBackground
+  : weather?.data?.current
+    ? getWeatherConditionBackground(weather.data.current.weather_code)
+    : "/weather/background/cloud.jpg";
     return(
         <>
         
-        <main className={` backdrop-brightness-[10%] bg-center bg-cover  min-h-dvh`}
+        <main className={` backdrop-brightness-[10%] bg-cover bg-fixed bg-center bg-no-repeat  min-h-dvh pb-24`}
                 style={{
                     
-                    backgroundImage: `url(${
-                    weather?.data?.current
-                        ? getWeatherConditionBackground(weather.data.current.weather_code)
-                        : "/weather/background/cloud.jpg"
+                    backgroundImage: `url(${bgImage
                     })`
                     
                 }}>
@@ -241,6 +268,7 @@ export default function Weather(){
      
                  </div>                 
             </CenterAll>
+        
             <Glass1 className="w-[95vw] mx-auto rounded-2xl">
               <div className="w-full h-fit p-2 overflow-x-hidden">
                     <span className=" flex flex-row text-gray-200 pb-1 border-b-1 border-gray-600  font-semibold  "><Clock4 className="mr-1.5"/> Hourly Forecast</span>
@@ -257,6 +285,8 @@ export default function Weather(){
                     </div>
                 </div>
             </Glass1>
+
+
             <Glass1 className="mt-5 w-[95vw] mx-auto rounded-2xl">
                 <div className="w-full h-fit  p-2  overflow-x-hidden">
                     <span className=" flex flex-row text-gray-200 font-semibold pb-1 border-b-1 border-gray-600 "><Calendar1 size={20} className="mr-1.5"/> 10-day forecast</span>
@@ -276,6 +306,129 @@ export default function Weather(){
                 </div>
 
             </Glass1>
+            <Glass1 className=" rounded-2xl p-4 mt-5 w-[95vw] mx-auto">
+                <span className=" flex flex-row text-gray-200 pb-1 border-b-1 border-gray-600  font-semibold  "><Thermometer className="mr-1.5"/> Temperature Chart</span>
+
+                <div className="w-full h-64 pt-4">
+                    {chartData.length > 0 ? (
+                    <ResponsiveContainer width="100%" height="100%">
+                        <AreaChart 
+                        data={chartData}
+                        margin={{ top: 10, right: 10, left: -20, bottom: 0 }}
+                        >
+                        <defs>
+                            <linearGradient id="colorTemp" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#3b82f6" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#3b82f6" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <CartesianGrid strokeDasharray="3 3" stroke="#ffffff20" />
+                        <XAxis 
+                            dataKey="time" 
+                            stroke="#cbd5e1" 
+                            fontSize={12} 
+                            tickLine={false}
+                            
+                        />
+                        <YAxis 
+                            stroke="#cbd5e1" 
+                            fontSize={12} 
+                            unit="°" 
+                            // domain={['auto', 'auto']}
+                            axisLine={false}
+                            tickLine={false}
+                            domain={[0, '13']}
+                        />
+                        <Tooltip
+                            contentStyle={{
+                            backgroundColor: "#0f172a",
+                            borderColor: "#334155",
+                            borderRadius: "0.5rem",
+                            color: "#fff",
+                            }}
+                        />
+                        <Area
+                            type="monotone"
+                            dataKey="temp"
+                            name="Temperatura"
+                            stroke="#60a5fa"
+                            strokeWidth={3}
+                            fillOpacity={1}
+                            fill="url(#colorTemp)"
+                        />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                    ) : (
+                    <div className="w-full h-full flex justify-center items-center text-white">
+                        <LoaderCircle className="animate-spin mr-2" size={20} />
+                    </div>
+                    )}
+                </div>
+            </Glass1>
+            <div className="w-[95vw] grid grid-cols-2 gap-3 my-4 mx-auto" >
+                <Glass1 className="w-full h-36 rounded-3xl border border-white/15 bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-xl shadow-xl overflow-hidden relative">
+                    <div className="p-4 flex flex-col justify-between h-full">
+                    <div className="flex items-center justify-between">
+                        <span className="px-2.5 py-1 rounded-full text-xs font-semibold bg-indigo-500/20 text-indigo-200 border border-indigo-500/30 flex items-center gap-1.5">
+                        <Gauge size={13} /> Ciśnienie
+                        </span>
+                    </div>
+                    <div className="text-center my-auto">
+                        <span className="text-4xl sm:text-5xl font-black text-white tracking-tighter drop-shadow-md">
+                        {weather?.data?.current?.pressure || "..."}
+                        </span>
+                        <span className="text-xs font-bold text-indigo-300 ml-1 uppercase">hPa</span>
+                    </div>
+                    </div>
+                </Glass1>
+                <Glass1 className="w-full h-36 rounded-3xl border border-white/15 bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-xl shadow-xl overflow-hidden relative">
+                    <div className="p-4 flex flex-col justify-between h-full">
+                    <div className="flex items-center justify-between">
+                        <span className="px-2.5 py-1 rounded-full text-xs font-semibold bg-yellow-300/20 text-yellow-300 border-yellow-300/30 flex items-center gap-1.5">
+                        <Gauge size={13} /> Wiatr
+                        </span>
+                    </div>
+                    <div className="text-center my-auto">
+                        <span className="text-4xl sm:text-5xl font-black text-white tracking-tighter drop-shadow-md">
+                        {weather?.data.current.wind.direction || "..."}°
+                        </span>
+                        <span className="text-xs font-bold text-yellow-300 ml-1 uppercase">{getWindDirection(weather?.data.current.wind.direction)}</span>
+                    </div>
+                    </div>
+                </Glass1>
+                <Glass1 className="w-full h-36 rounded-3xl border border-white/15 bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-xl shadow-xl overflow-hidden relative">
+                    <div className="p-4 flex flex-col justify-between h-full">
+                    <div className="flex items-center justify-between">
+                        <span className="px-2.5 py-1 rounded-full text-xs font-semibold bg-red-500/20 text-red-200 border border-red-500/30 flex items-center gap-1.5">
+                        <Thermometer size={13} /> Odczuwalna
+                        </span>
+                    </div>
+                    <div className="text-center my-auto">
+                        <span className="text-4xl sm:text-5xl font-black text-white tracking-tighter drop-shadow-md">
+                        {weather?.data?.current?.feels_like || "..."}°C
+                        </span>
+                        <span className="text-xs font-bold text-indigo-300 ml-1 uppercase"></span>
+                    </div>
+                    </div>
+                </Glass1>
+                <Glass1 className="w-full h-36 rounded-3xl border border-white/15 bg-gradient-to-br from-white/10 to-white/5 backdrop-blur-xl shadow-xl overflow-hidden relative">
+                    <div className="p-4 flex flex-col justify-between h-full">
+                    <div className="flex items-center justify-between">
+                        <span className="px-2.5 py-1 rounded-full text-xs font-semibold bg-blue-500/20 text-blue-200 border border-blue-500/30 flex items-center gap-1.5">
+                        <CloudRainWind size={13} /> Deszcz
+                        </span>
+                    </div>
+
+                    <div className="text-center my-auto">
+                        <span className="text-4xl sm:text-5xl font-black text-white tracking-tighter drop-shadow-md">
+                        {weather?.data?.forecast?.[0]?.precipitation_probability ?? "..."}
+                        </span>
+                        <span className="text-xs font-bold text-blue-300 ml-1 uppercase">%</span>
+                    </div>
+                    </div>
+                </Glass1>
+
+            </div>
         </main>
         
         </>
